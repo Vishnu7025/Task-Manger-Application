e8d2a45f7690b568129ed2e607e525a877caeeba
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-03 19:17:27 +0530

    url and router

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from todo import views
+from rest_framework import routers
 
-
+router = routers.DefaultRouter()
+router.register(r'tasks',views.TodoView,'task')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',include(router.urls))
 ]
