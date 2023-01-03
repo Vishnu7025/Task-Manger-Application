68f4ab200603b68c7ae663b3a3ee95c95b0e28cb
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-03 19:13:42 +0530

    creating endpoint

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .serializers import TodoSerializer
+from rest_framework import viewsets
+from . models import Todo
 
-# Create your views here.
+class TodoView(viewsets.ModelViewSet):
+    serializer_class = TodoSerializer
+    queryset = Todo.objects.all
