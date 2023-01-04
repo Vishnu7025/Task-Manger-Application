ec400280e32ef5a25d30ef6a2878e6eeb2e8157e
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2023-01-04 19:59:58 +0530

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,19 +3,19 @@ import axios from 'axios'
 import  {useState} from 'react'
 
 function Home() {
-    const [state,setSate]= useState([])
+    const [state,setState]= useState([])
   return (
     <div className='container'>
         <button  onClick={()=>{
               axios.get('http://127.0.0.1:8000/api/tasks/').then((response)=>{
                 console.log(response.data)
-                setSate(response.data)
+                setState(response.data)
             }) 
         }}>Click here</button>
         {state.map((obj,index)=>{
             return(
                 <div>
-                    <h1>{index}</h1>
+                    <h1>{index+1}</h1>
                     <h4>{obj.title}</h4>
                     <h4>{obj.description}</h4>
                 </div>
